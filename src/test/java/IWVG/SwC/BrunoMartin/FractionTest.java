1ff59d3fcbfa864d3baea5a84784698fc0fcba15
Bruno Martin <=>
2018-09-20 16:53:01 +0200

    Initial commit. Architect Bruno Martín López

---
+++ src/test/java/IWVG/SwC/BrunoMartin/FractionTest.java
@@ -0,0 +1,32 @@
+package IWVG.SwC.BrunoMartin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FractionTest {
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        this.fraction = new Fraction();
+    }
+
+    @Test
+    void testFractionIntInt (){
+        this.fraction = new Fraction(2,2);
+        assertEquals(2,fraction.getNumerator());
+        assertEquals(2,fraction.getDenominator());
+    }
+    @Test
+    void testFraction (){
+        assertEquals(1,fraction.getNumerator());
+        assertEquals(1,fraction.getDenominator());
+    }
+
+    @Test
+    void testDecimal (){
+             assertEquals(1,this.fraction.decimal(),10e-5);
+    }
+}
